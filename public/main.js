b447b1bc72949e018d99f776477d6ca7021fc9f3
Grobbies26 <74908721+Grobbies26@users.noreply.github.com>
2022-07-11 11:33:04 +0200

    🎨 (electron) Application is now standalone executable

---
+++ public/main.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow}  =  require('electron')
+
+require('@electron/remote/main').initialize()
+
+function createWindow( ) {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            enableRemoteModule: true
+        }
+    })
+
+    window.loadURL('http://localhost:3000')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', function(){
+    if(process.platform !== 'darwin'){
+        app.quit()
+    }
+})
+
+app.on('activate', function(){
+    if(BrowserWindow.getAllWindows().length === 0){
+        createWindow()
+    }
+})
